06403f575b33469fc8e3bd8700b657e19840b683
kfoxone <raphaelhendrick@gmail.com>
2024-03-07 16:56:00 +0100

    Création du projet de formaton avec les fichiers de bases : - pom : fichier de configuration, - Bases : première class

---
+++ base/src/main/java/org/blc/training/base/Base.java
@@ -0,0 +1,14 @@
+package org.blc.training.base;
+
+/**
+ * Base est la class ou nous allons apprendre à utiliser la class des évènement
+ * de sortie
+ *
+ * @author r.hendrick
+ */
+public class Base {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
